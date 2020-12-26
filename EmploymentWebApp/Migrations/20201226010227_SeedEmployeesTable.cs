ed339937bcc6cd02133871546414418ab356710f
HusseinYoussef <husseinymousa@gmail.com>
2020-12-26 15:51:50 +0200

    Add Migrations

---
+++ EmploymentWebApp/Migrations/20201226010227_SeedEmployeesTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmploymentWebApp.Migrations
+{
+    public partial class SeedEmployeesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 1, 1, "Hussein@gmail.com", "Hussein" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 1, "Hassan@gmail.com", "Hassan" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
