ee3adf0332858af0333d115375efba482a45af81
HusseinYoussef <husseinymousa@gmail.com>
2020-12-26 15:48:13 +0200

    CRUD Action Methods - Implemented Create, Get, Edit, Delete action methods.

--- EmploymentWebApp/Controllers/HomeController.cs
+++ EmploymentWebApp/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace EmploymentWebApp.Controllers
 {
     public class HomeController: Controller
     {
-        private IEmployeeRepository _employeeRepository;
+        private readonly IEmployeeRepository _employeeRepository;
         public HomeController(IEmployeeRepository employeeRepository)
         {
             _employeeRepository = employeeRepository;
@@ -19,14 +19,55 @@ namespace EmploymentWebApp.Controllers
             var model = _employeeRepository.GetAllEmployees();
             return View(model);
         }
-        public ViewResult Details()
+        public ViewResult Details(int? id)
         {
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                Employee = _employeeRepository.GetEmployee(1),
+                Employee = _employeeRepository.GetEmployee(id??1),
                 PageTitle = "Employee Details"
             };
             return View(homeDetailsViewModel);
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee changedEmployee)
+        {
+            if(ModelState.IsValid)
+            {
+                // Employee oldEmployee = _employeeRepository.GetEmployee(changedEmployee.Id);
+                _employeeRepository.UpdateEmployee(changedEmployee);
+                return RedirectToAction("details", new {id=changedEmployee.Id});
+            }
+            return View(changedEmployee);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                int employeeId = _employeeRepository.AddEmployee(employee);
+                return RedirectToAction("details", new {id=employeeId});
+            }
+            return View();
+        }
+
+        public RedirectToActionResult Delete(int id)
+        {
+            _employeeRepository.DeleteEmployee(id);
+            return RedirectToAction("index");
         }
     }
 }
