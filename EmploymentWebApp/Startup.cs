3868299fb4db6b41e7c9eedc5a6a62a00dae9ee6
HusseinYoussef <husseinymousa@gmail.com>
2020-12-26 15:47:20 +0200

    Configured Services and Request Pipeline.

--- EmploymentWebApp/Startup.cs
+++ EmploymentWebApp/Startup.cs
@@ -5,36 +5,60 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using EmploymentWebApp.Models;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmploymentWebApp
 {
     public class Startup
     {
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+            services.AddMvc(delegate(MvcOptions options) {options.EnableEndpointRouting=false;})
+                    .AddXmlSerializerFormatters();
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+                            ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
+                developerExceptionPageOptions.SourceCodeLineCount=3;
+                // Plugged as early as possible
+                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
             }
 
-            app.UseRouting();
+            // app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+            app.UseStaticFiles();
+            // app.UseMvcWithDefaultRoute();
+            
+            // Conventional Routing
+            app.UseMvc(route => route.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"));
+
+            app.Run(async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+                    await context.Response.WriteAsync("Hello World");
+                }
+            );
         }
     }
 }
